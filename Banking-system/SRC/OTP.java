bdc4c59693c8840d05f8c9b4255f4ce943019cd4
Shanmugar Rathinasamy Mariappan <shanmugar_rm@yahoo.com>
2016-05-15 17:21:42 -0400

    Update OTP.java

--- Banking-system/SRC/OTP.java
+++ Banking-system/SRC/OTP.java
@@ -9,17 +9,8 @@ public class OTP {
 		  Random randomGenerator = new Random();
 		  
 		  int randomInt = randomGenerator.nextInt(899999)+100000;
-		  
-		  System.out.println("In otp class");
-		  //Integer obj1 = new Integer(2009);
-		 // String obj2 = new String(randomInt);
+
 		  String obj2 = String.valueOf(randomInt);
-		 // System.out.println("hashCode for an integer is " + obj1.hashCode());
-		  System.out.println("hashCode for a string is " + obj2.hashCode());
-		  
-		
 		return randomInt;
 	}
 }
-
- 
